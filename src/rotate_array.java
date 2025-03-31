c76f95e46ba21f6185652f6f93fc8cfea8320556
vicky-ogutu <vikogutu77@gmail.com>
2025-04-01 02:26:29 +0300

    rotate an array

---
+++ src/rotate_array.java
@@ -0,0 +1,13 @@
+public class rotate_array {
+    public static void main(String []args){
+        int[]x ={1,2,3,4,5};
+        rotate(x);
+    }
+
+    public static void rotate(int[]arr){
+
+        for (int i= arr.length; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
