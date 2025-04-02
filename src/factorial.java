abd6c968e1d07d16db2de96329461a7a1619221e
vicky-ogutu <vikogutu77@gmail.com>
2025-04-03 02:10:08 +0300

    permutation

---
+++ src/factorial.java
@@ -0,0 +1,15 @@
+public class factorial {
+
+    public static void main(String[]args){
+
+        String name="abcd";
+        int length =name.length();
+
+        int factorial=1;
+
+        for (int i=1; i<=length; i++){
+            factorial  *=i;
+        }
+        System.out.println(factorial);
+    }
+}
