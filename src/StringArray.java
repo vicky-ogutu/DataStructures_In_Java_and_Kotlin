6c4c1ede4289c807b73cbeba2762d6657784f27b
vicky-ogutu <vikogutu77@gmail.com>
2025-04-05 20:17:40 +0300

    Stringify_Array

---
+++ src/StringArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class StringArray {
+    public static void main(String[] args) {
+        String[] cars =new String[4];
+        cars[0]= "Allion";
+        cars[1]="Premio";
+        cars[2]="Impreza";
+        System.out.println(Arrays.toString(cars));
+
+
+    }
+}
